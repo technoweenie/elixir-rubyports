60840b48c44e12fbbbf0de663cc44a9510c7b50a
rick <technoweenie@gmail.com>
2012-03-02 00:03:45 -0700

    implement read/1, read/2/, read/3

--- lib/file_utils.ex
+++ lib/file_utils.ex
@@ -4,21 +4,45 @@ defmodule FileUtils do
   end
 
   def pwd(drive) do
-    path = if drive do
+    {:ok, dir} = if drive do
       :file.get_cwd(drive)
     else:
       :file.get_cwd
     end
 
-    case path do
-    match: {:ok, dir}
-      dir
+    dir
+  end
+
+  def read(filename) do
+    {:ok, data} = :file.read_file(filename)
+    data
+  end
+
+  def read(filename, cb) when is_function(cb) do
+    read(filename, [:raw], cb)
+  end
+
+  def read(filename, mode, cb) when is_function(cb) do
+    {:ok, io} = :file.open(filename, mode)
+    read_file_chunk(io, false, cb)
+  end
+
+  defp read_file_chunk(io, acc, cb) do
+    res = :file.read(io, 16384)
+    case res do
+    match: {:ok, data}
+      if is_function(cb, 1) do
+        cb.(data)
+      else:
+        acc = cb.(acc || [], data)
+      end
+      read_file_chunk(io, acc, cb)
     match: {:error, reason}
-      IO.puts "Error: #{reason}"
-      nil
-    else:
-      IO.puts "doh!"
-      nil
+      :file.close(io)
+      res
+    match: :eof
+      :file.close(io)
+      acc
     end
   end
 end
